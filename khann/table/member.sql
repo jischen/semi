4e92fb4819c009b1f669a4f16e1b5def33d45c83
jischen <jischen@naver.com>
2020-07-03 16:47:50 +0900

    member table,seq 완료

---
+++ khann/table/member.sql
@@ -0,0 +1,18 @@
+
+CREATE TABLE member (
+member_no number PRIMARY KEY,
+member_id varchar2(16) NOT NULL UNIQUE CHECK(LENGTH(member_id) BETWEEN 5 AND 20) ,
+member_pw varchar2(20) NOT NULL UNIQUE CHECK(LENGTH(member_pw) BETWEEN 8 AND 16),
+member_name varchar2(21) NOT NULL,
+post char(5) NOT null ,
+base_addr varchar2(300) ,
+extra_addr varchar2(300) ,
+member_birth DATE NOT NULL ,
+member_phone char(11) NOT NULL ,
+grade varchar2(9) NOT NULL check(grade in('Silver', 'Gold', 'VIP', '관리자')),
+member_join DATE  DEFAULT sysdate NOT NULL,
+member_login DATE     -- null / sysdate가 아니기 때문에
+);
+
+CREATE SEQUENCE member_seq nocache;
+
