aef6829b6aad7f4ea06c736ccb6113bc0a2b1add
627sung <627sung@naver.com>
2020-07-03 18:12:36 +0900

    theaterSql

---
+++ khann/table/theater.sql
@@ -0,0 +1,26 @@
+
+
+
+
+<!--상영관 생성 테이블 -->
+
+CREATE TABLE Theater(
+Theater_no NUMBER PRIMARY KEY, 
+Theater_name char(5) NOT NULL,
+Theater_type varchar2(30) NOT null,
+Theater_row NUMBER NOT null, 
+Theater_col NUMBER NOT null 
+);
+
+<!--좌석  생성 테이블 -->
+
+CREATE TABLE seat(
+seat_no NUMBER PRIMARY KEY,
+Theater_no REFERENCES Theater(Theater_no) ON DELETE CASCADE,
+seat_type varchar2(20) check(seat_type IN ('커플석', '장애인석', '일반석')),
+seat_row char(1) NOT NULL,
+seat_col NUMBER NOT NULL
+);
+
+
+
