ab652dde62507653440eb11a2ad2e90319bc6b52
roh-dh <ndh13399@naver.com>
2020-07-06 11:55:52 +0900

    1. META-INF에 을 만들어놨으니 context.xml 잘들어왔는확인 2. WebContent-WEB-INF-lib에 commons-dbcp2-2.7.0jar 넣으세요 3. DAO에 카톡보낸대로 등록하세요

--- khann/src/beans/dao/MemberDao.java
+++ khann/src/beans/dao/MemberDao.java
@@ -1,5 +1,38 @@
 package beans.dao;
 
-public class MemberDao {
+import java.sql.Connection;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+public class MemberDao {
+	//context.mxl에서 관리하는 자원 객체를 참조할 수 있도록 연결 코드 구현
+		private static DataSource src; //리모컨 선언
+		
+		//static 변수의 초기화가 복잡할 경우에 사용할 수 있는 static 전용 구문
+		static {
+			//src = context.xml에서 관리하는 자원의 정보
+			try {
+				Context ctx = new InitialContext();//탐색 도구
+				Context env = (Context)ctx.lookup("java:/comp/env");//context 설정 탐색
+				src = (DataSource) env.lookup("jdbc/oracle");
+			} catch (NamingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} //탐색 도구
+		}
+			
+		
+		//연결 메소드
+		public Connection getConnection() throws Exception {
+//			Class.forName("oracle.jdbc.OracleDriver");
+//			return DriverManager.getConnection(
+//					"jdbc:oracle:thin:@localhost:1521:xe", "c##kh", "c##kh");
+			return src.getConnection();
+
+		}
+	
+	
 }
