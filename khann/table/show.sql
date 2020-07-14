164686b7f58c6e27f5cc6d060a4e5c8c8ff23654
ohyoona <yoona3827@naver.com>
2020-07-14 16:36:27 +0900

    ㄹㅇㄴㄹ

--- khann/table/show.sql
+++ khann/table/show.sql
@@ -2,7 +2,9 @@ CREATE TABLE show (
 show_no NUMBER PRIMARY KEY, --시퀀스 
 movie_no NUMBER REFERENCES movie(movie_no) ON DELETE CASCADE, --무비노 참조키
 theater_no NUMBER REFERENCES theater(theater_no) ON DELETE CASCADE, --참조키
-show_start varchar2(20) NOT NULL
+show_start date NOT NULL  --date추가
 );
- 
+
+
+
 CREATE SEQUENCE show_seq nocache;
