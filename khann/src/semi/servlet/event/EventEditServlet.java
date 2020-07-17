9d104348be1ae1e2d139a9297207a4d3f4ab8c4d
jischen <jischen@naver.com>
2020-07-17 23:06:42 +0900

    수정해!!!

--- khann/src/semi/servlet/event/EventEditServlet.java
+++ khann/src/semi/servlet/event/EventEditServlet.java
@@ -1,6 +1,10 @@
 package semi.servlet.event;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.List;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,8 +12,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+
 import beans.dao.EventDao;
+import beans.dao.EventFileDao;
 import beans.dto.EventDto;
+import beans.dto.EventFileDto;
 
 @WebServlet(urlPatterns="/event/edit.do")
 public class EventEditServlet extends HttpServlet {
@@ -17,22 +27,77 @@ public class EventEditServlet extends HttpServlet {
 		try {
 				
 			req.setCharacterEncoding("UTF-8");
+			int limit = 10*1024*1024;
+			File baseDir = new File("D:/upload/event");
 			
-			EventDto edto=new EventDto();
-			edto.setEvent_no(Integer.parseInt(req.getParameter("event_no")));
-			edto.setEvent_title(req.getParameter("event_title"));
-			edto.setEvent_content(req.getParameter("event_content"));
-			edto.setEvent_condition(req.getParameter("event_condition"));
+			DiskFileItemFactory factory = new DiskFileItemFactory(limit,baseDir);
+			factory.getDefaultCharset(charset);
 			
-			EventDao edao=new EventDao();
-			edao.edit(edto);
+			ServletFileUpload utility = new ServletFileUpload(factory);
 			
-			resp.sendRedirect("content.jsp?event_no="+edto.getEvent_no());
+			 Map<String, List<FileItem>> map = utility.parseParameterMap(req);
 			
-		}
-		catch(Exception e) {
-			e.printStackTrace();
-			resp.sendError(500);
+			 EventDto edto = new EventDto();
+			 
+			 edto.setEvent_title(map.get("event_title").get(0).getString());
+			 edto.setEvent_content(map.get("event_content").get(0).getString());
+			 
+			 EventDao edao = new EventDao();
+			 int event_no = Integer.parseInt(map.get("event_no").get(0).getString());
+			 edto.setEvent_no(event_no);
+		
+			 edao.edit(edto);
+			 
+			 List<FileItem> fileList = map.get("event_file_no");
+			 
+			 for(FileItem item : fileList) {
+			 			 
+				 if(item.getSize()>0) {
+				
+				EventFileDao efdao = new EventFileDao();
+				EventFileDto efdto = new EventFileDto();
+			
+				 int event_file_no = efdao.getSequence();
+				 efdto.setEvent_file_no(event_file_no);
+				 efdto.setEvent_file_name(item.getName());
+				 efdto.setEvent_file_size(item.getSize());
+				 efdto.setEvent_file_type(item.getContentType());
+				 efdao.save(efdto);
+				 
+				 EventFileDto efdto = new EventFileDto();
+				 efdto.setEvent_file_origin(event_file_origin);
+				 efdto.setEvent_file_no(event_file_origin);
+				 
+				 EventFileDao efdao = new EventFileDao();
+		               efdao.save(efdto);
+		               
+		               File target = new File(baseDir, String.valueOf(file_no));
+		               item.write(target);
+
+		               resp.sendRedirect("Eventcontent.jsp?event_no="+event_no);
+		               
+			      } catch (Exception e) {
+			         // TODO Auto-generated catch block
+			         e.printStackTrace();
+			         resp.sendError(500);
+			      }
+			   }
+			}
+				 
+				 }
+	/*
+	 * EventDto edto=new EventDto();
+	 * edto.setEvent_no(Integer.parseInt(req.getParameter("event_no")));
+	 * edto.setEvent_title(req.getParameter("event_title"));
+	 * edto.setEvent_content(req.getParameter("event_content"));
+	 * edto.setEvent_condition(req.getParameter("event_condition"));
+	 * 
+	 * EventDao edao=new EventDao(); edao.edit(edto);
+	 * 
+	 * resp.sendRedirect("content.jsp?event_no="+edto.getEvent_no());
+	 * 
+	 * } catch(Exception e) { e.printStackTrace(); resp.sendError(500);
+	 */
 		}
 	}
 }
